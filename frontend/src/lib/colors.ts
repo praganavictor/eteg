352d35a1bcbf30801bfad5e653ab29e27271474f
Victor Pragana <victorpragana@tickpay.com>
2024-12-16 06:52:23 -0300

    add frontend

---
+++ frontend/src/lib/colors.ts
@@ -0,0 +1,10 @@
+export const colors = [
+    { value: 'Red', label: 'Vermelho' },
+    { value: 'Orange', label: 'Laranja' },
+    { value: 'Yellow', label: 'Amarelo' },
+    { value: 'Green', label: 'Verde' },
+    { value: 'Blue', label: 'Azul' },
+    { value: 'Indigo', label: 'Indigo' },
+    { value: 'Violet', label: 'Violeta' },
+  ];
+  
